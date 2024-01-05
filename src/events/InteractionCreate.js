477ec4abdbe1566c76ae4212b3b78b8bc15df0fa
Ayomide <ayomidekenny952@gmail.com>
2024-01-05 08:52:56 -0800

    using html

--- src/events/InteractionCreate.js
+++ src/events/InteractionCreate.js
@@ -1,10 +1,48 @@
 const { Events } = require("discord.js");
+const { readFile } = require('fs');
+const { join } = require('path');
+
 
 module.exports = {
     name: Events.InteractionCreate,
     async execute (interaction) {
         if (!interaction.isChatInputCommand()) {
-            interaction.followUp("Please use the '/gen' to generate image.");
+            interaction.followUp(`<head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>WeaCanion - bot</title>
+            <style>
+                body {
+                    font-family: Verdana, Geneva, Tahoma, sans-serif;
+                    margin: 10px 20px;
+                }
+                .container {
+                    width: 100%;
+                    height: 100%;
+                    max-width: 400px;
+                    border: 5px solid rgba(30, 143, 255, 0.236);
+                    display: flex;
+                    flex-direction: column;
+                    justify-content: space-around;
+                    background-color: chocolate;
+                }
+                .inner-container {
+                    padding: .28rem;
+                }
+                .info {
+                    color: #ccc;
+                    font-size: 14px;
+                }
+            </style>
+        </head>
+        <body>
+            <div class="container">
+                <div class="inner-container">
+                    <p class="icon">😉</p>
+                    <p class="info">Please use the '/gen' command to generate image.</p>
+                </div>
+            </div>
+        </body>`);
             return;
         };
 
